39f32e4eb88b3fa181d0981c24d19e97b5e275ad
curder <q.curder@gmail.com>
2022-07-27 14:41:45 +0800

    using crawl template spider

--- dushu.com/README.md
+++ dushu.com/README.md
@@ -2,8 +2,18 @@
 
  获取[读书网](https://www.dushu.com/book/1617.html)中"国学入门"书籍列表的详情信息。
  
+## 构建蜘蛛
+
+```bash
+scrapy genspider ds dushu.com  # 构建普通爬虫
+scrapy genspider -t crawl ds2 dushu.com # 构建带链接提取器的爬虫
+```
+
+
 运行
 
 ```bash
-scrapy crawl ds
+scrapy crawl ds  # 使用普通爬虫
+
+scrapy crawl ds2  # 使用链接提取器爬虫
 ```
---
+++ dushu.com/dushu/spiders/ds2.py
@@ -0,0 +1,36 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class Ds2Spider(CrawlSpider):
+    name = 'ds2'
+    allowed_domains = ['dushu.com']
+    start_urls = ['https://www.dushu.com/book/1617.html']
+
+    rules = (
+        # 提取详情页链接
+        Rule(LinkExtractor(allow=r'/book/\d+/'), callback='parse_item', follow=False),
+
+        # 提取分页页码列表链接
+        Rule(LinkExtractor(allow=r'/book/1617_\d+\.html'), follow=True),
+    )
+
+    def parse_item(self, response):
+        id = response.url.strip('/').split('/')[-1]
+        title = response.xpath('//div[@class="book-title"]/h1/text()').get()
+        image_url = response.xpath('//div[@class="book-pic"]/div[@class="pic"]/img/@src').get()
+        price = response.xpath('//p[@class="price"]/span[@class="num"]/text()').get('0')
+        author = response.xpath('//div[@class="book-details-left"]/table/tbody/tr[1]/td[2]/text()').get('')
+        publishing_house = response.xpath('//div[@class="book-details-left"]/table/tbody/tr[2]/td[2]/text()').get()
+        description = response.xpath("//div[@class='book-summary'][1]//div[@class='text txtsummary']/text()").get('')
+
+        yield dict(
+            id=id,
+            title=title,
+            image_url=image_url,
+            price=price,
+            author=author,
+            publishing_house=publishing_house,
+            description=description,
+        )
