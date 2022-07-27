6a1931a791c2b39fbf2ec89dd58fcb0e4d7d68ec
curder <q.curder@gmail.com>
2022-07-27 11:30:57 +0800

    get data from response use xpath and get method.

--- scrapy_basic_demo/scrapy_basic_demo/spiders/get.py
+++ scrapy_basic_demo/scrapy_basic_demo/spiders/get.py
@@ -7,4 +7,6 @@ class GetSpider(scrapy.Spider):
     start_urls = ['http://baidu.com/']  # 起始URL，可以配置多个，当爬虫开始时，列表中的URLs将封装成球球对象提交给引擎
 
     def parse(self, response):  # 解析函数，用于解析爬取数据的内容，当引擎回传给Spider一个响应对象时触发回调
-        print(response.url)
+        title = response.xpath('//input[@id="su"]/@value')  # 通过Xpath获取响应内容，返回 Selector 对象
+        print(title.get())  # Selector对象使用 `get()` 或者 `getall()` 获取内容
+
