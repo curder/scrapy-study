234b44ab0e88072bb32a24cad88a8ceb9ec2c1e6
curder <q.curder@gmail.com>
2022-07-27 11:39:30 +0800

    Open pipeline setting

--- scrapy_basic_demo/scrapy_basic_demo/settings.py
+++ scrapy_basic_demo/scrapy_basic_demo/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapy_basic_demo.spiders'
 #USER_AGENT = 'scrapy_basic_demo (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapy_basic_demo.pipelines.ScrapyBasicDemoPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapy_basic_demo.pipelines.ScrapyBasicDemoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
